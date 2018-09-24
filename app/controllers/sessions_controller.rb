c7dd4356001af373b74ed912f652ad9a9d9dfd42
Trac Nguyen <nguyendtrac@gmail.com>
2018-09-24 04:33:28 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,12 +2,16 @@ class SessionsController < ApplicationController
   def new
 
   end
-  
+
   def create
     if params[:name].nil? || params[:name].empty?
       redirect_to controller: 'sessions', action: 'new'
     else
       session[:name] = params[:name]
     end
+  end
+
+  def destroy
+
   end
 end
