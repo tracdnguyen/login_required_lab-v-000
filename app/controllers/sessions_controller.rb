24f188b671fd6f20416214fb9887cb5200696bf7
Trac Nguyen <nguyendtrac@gmail.com>
2018-09-24 04:35:13 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
       redirect_to controller: 'sessions', action: 'new'
     else
       session[:name] = params[:name]
+      redirect_to controller: 'application', action: 'hello'
     end
   end
 
